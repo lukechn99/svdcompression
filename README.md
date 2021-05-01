94dc6f1e5332a0a498ea4b0b2e6646f1ed36d0f7
Luke Chen <lukechn99@gmail.com>
2021-04-30 21:30:54 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # svdcompression
+
+Uses SVD matrix decomposition to create compressed media
+
+### todo
+import directory for compression  
+compression multithreading to speed up for multiple files  
+bit interpreter for decompressing new filetype .spd  
+error correction bits  
+
+### references
+https://www.geeksforgeeks.org/python-pil-image-convert-method/  
+https://www.pythonforthelab.com/blog/storing-binary-data-and-serializing/  
+https://www.frankcleary.com/svdimage/  
+https://mashable.com/2012/10/22/zip-files/  
+https://blog.tensorflow.org/2020/02/matrix-compression-operator-tensorflow.html  
---
+++ compress.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from PIL import Image
+import os
+import sys
+
+
+def compressImg(filename):
+    img = Image.open(filename)
+    imggray = img.convert('LA')
+    imggray.show()
+    plt.figure(figsize=(9, 6))
+    plt.imshow(imggray)
+
+    imgmat = np.array(list(imggray.getdata(band=0)), float)
+    imgmat.shape = (imggray.size[1], imggray.size[0])
+    imgmat = np.matrix(imgmat)
+    plt.figure(figsize=(9, 6))
+    plt.imshow(imgmat, cmap='gray')
+    plt.show()
+
+    U, sigma, V = np.linalg.svd(imgmat)
+
+    print(U)
+    print(sigma)
+    print(V)
+
+    reconstimg = np.matrix(U[:, :1]) * np.diag(sigma[:1]) * np.matrix(V[:1, :])
+    plt.imshow(reconstimg, cmap='gray')
+
+    for i in range(2, 4):
+        reconstimg = np.matrix(U[:, :i]) * \
+            np.diag(sigma[:i]) * np.matrix(V[:i, :])
+        plt.imshow(reconstimg, cmap='gray')
+        title = "n = %s" % i
+        plt.title(title)
+        plt.show()
+
+    for i in range(5, 51, 5):
+        reconstimg = np.matrix(U[:, :i]) * \
+            np.diag(sigma[:i]) * np.matrix(V[:i, :])
+        plt.imshow(reconstimg, cmap='gray')
+        title = "n = %s" % i
+        plt.title(title)
+        plt.show()
+
+
+def compressTxt(filename):
+    pass
+
+
+def compress(dir):
+    print(dir)
+    tempcwd = os.getcwd()
+    if os.path.isdir(os.path.join(tempcwd, dir)):
+        for d in os.listdir(os.path.join(tempcwd, dir)):
+            os.chdir(os.path.join(tempcwd, dir))
+            extension = d.split(".")[1]
+            if extension == "txt":
+                compressTxt(d)
+            elif extension == "jpg" or extension == "png":
+                compressImg(d)
+            else:
+                print("filetype not supported, " + d + " was not compressed")
+            compress(d)
+            os.chdir(tempcwd)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("use file")
+    else:
+        compress(str(sys.argv[1]))
+
+
+if __name__ == "__main__":
+    main()
